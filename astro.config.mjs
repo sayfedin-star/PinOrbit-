b225b6f9c327a045f8bd6d954af7bc4905a7bff6
sayfedin-star <sayfedin@gmail.com>
2026-08-05 16:40:57 +0100

    feat: add account management pages and @astrojs/cloudflare integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'static',
+  adapter: cloudflare({
+    imageService: 'passthrough',
+  }),
+  output: 'hybrid',
 });
